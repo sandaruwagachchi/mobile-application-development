999421839306955032fc97887d92abe71d3ace7c
Sandaru Wagachchi <151208394+sandaruwagachchi@users.noreply.github.com>
2025-05-29 17:45:53 +0530

    Fragments

---
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.fragment
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        replaceFragment(HomeFragment())
+
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> replaceFragment(HomeFragment())
+                R.id.nav_profile -> replaceFragment(ProfileFragment())
+                R.id.nav_settings -> replaceFragment(SettingsFragment())
+            }
+            true
+        }
+    }
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentcontainer, fragment)
+            .commit()
+    }
+}
---
+++ Fragment/app/src/main/java/com/example/fragment/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.example.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SettingsFragment : Fragment(R.layout.fragment_settings)
